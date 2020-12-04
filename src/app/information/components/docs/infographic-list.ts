fdaacb174857f34f43556349a37dc266b0eca264
Zachary Bryant <Zachary@admiralfeb.net>
2020-12-03 20:28:06 -0600

    refactor: mv img

--- src/app/information/components/docs/infographic-list.ts
+++ src/app/information/components/docs/infographic-list.ts
@@ -1,5 +1,5 @@
 import CaveJohnson from '../../assets/Cave Johnson.png';
-import StationMap from '../../assets/ED station pad map.png';
+import StationMap from '../../assets/ED station landing pad map.png';
 import FSS from '../../assets/FSS-1.jpg';
 import Scoopable from '../../assets/KBGFoam.jpg';
 import Coriolis from '../../assets/Entrance_on_a_Coriolis_Dock.png';
