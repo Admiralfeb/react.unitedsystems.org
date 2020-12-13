61f223efaf74ef5df966742f3bd33d9b47651c77
Zachary Bryant <Zachary@admiralfeb.net>
2020-12-13 14:46:23 -0600

    refactor: mv app/index styling to theme.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import { StrictMode } from 'react';
-import './index.css';
-import App from './app';
+import { App } from './app';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
