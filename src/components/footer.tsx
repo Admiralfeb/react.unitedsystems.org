55412f8f805af4b97bb88df493535aee8147b81a
Zachary Bryant <Zachary@admiralfeb.net>
2021-01-02 21:50:38 -0600

    feat: basic auth

---
+++ src/components/footer.tsx
@@ -0,0 +1,79 @@
+import Typography from '@material-ui/core/Typography';
+import { Button, makeStyles } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import { ReactNode, useContext } from 'react';
+import { RealmAppContext } from 'providers';
+import { LoginProviders } from 'models/loginProviders';
+
+function ImgCopyright() {
+  return (
+    <Typography
+      variant="body2"
+      color="textSecondary"
+      style={{ textAlign: 'center' }}
+    >
+      Thank you to CMDR Pablok for the background image
+    </Typography>
+  );
+}
+function Copyright() {
+  return (
+    <Typography
+      variant="body2"
+      color="textSecondary"
+      style={{ textAlign: 'center' }}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://unitedsystems.org/">
+        United Systems Cooperative
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[200]
+        : theme.palette.grey[800],
+  },
+  center: {
+    textAlign: 'center',
+  },
+}));
+
+export const Footer = (props: { children?: ReactNode | JSX.Element }) => {
+  const classes = useStyles();
+  const realm = useContext(RealmAppContext);
+  const { children } = props;
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.main}>{children}</div>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm" className={classes.center}>
+          {realm?.checkUserProvider(LoginProviders.userpass) === true && (
+            <Button onClick={() => realm.logOut()}>Log out</Button>
+          )}
+          <Copyright />
+          <ImgCopyright />
+        </Container>
+      </footer>
+    </div>
+  );
+};
--- src/providers/realmAppProvider.tsx
+++ src/providers/realmAppProvider.tsx
@@ -1,5 +1,6 @@
 import { IRealmContext } from 'models/realmContext';
 import { createContext, ReactNode, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import * as Realm from 'realm-web';
 
 export const RealmAppContext = createContext<IRealmContext | null>(null);
@@ -13,25 +14,81 @@ export const RealmAppProvider = (props: {
     throw new Error('appId is not defined');
   }
   const [app, setApp] = useState(new Realm.App(appId));
+  const history = useHistory();
+
   useEffect(() => {
     setApp(new Realm.App(appId));
   }, [appId]);
 
   // Wrap the Realm.App object's user state with React state
   const [currentUser, setCurrentUser] = useState(app.currentUser);
-  async function logInAnon() {
+
+  const logInAnon = async () => {
     await app.logIn(Realm.Credentials.anonymous());
-    // If successful, app.currentUser is the user that just logged in
     setCurrentUser(app.currentUser);
-  }
-  async function logOut() {
+  };
+
+  const logInEmail = async (email: string, password: string) => {
+    const credentials = Realm.Credentials.emailPassword(email, password);
+    try {
+      const user: Realm.User = await app.logIn(credentials);
+      if (user.id === app.currentUser?.id) {
+        console.log('logged in as', currentUser);
+        setCurrentUser(app.currentUser);
+      } else {
+        throw new Error('Failed to login');
+      }
+    } catch (err) {
+      throw new Error(`Failed to login: ${err}`);
+    }
+  };
+
+  const logOut = async () => {
     // Log out the currently active user
     await app.currentUser?.logOut();
     // If another user was logged in too, they're now the current user.
     // Otherwise, app.currentUser is null.
     setCurrentUser(app.currentUser);
-  }
-  const wrapped = { app, currentUser, logInAnon, logOut };
+    history.push('/login');
+  };
+
+  const requestPasswordReset = async (email: string) => {
+    await app.emailPasswordAuth.sendResetPasswordEmail(email);
+  };
+
+  const completePasswordReset = async (
+    token: string,
+    tokenId: string,
+    newPass: string
+  ) => {
+    try {
+      await app.emailPasswordAuth.resetPassword(token, tokenId, newPass);
+    } catch (err) {
+      throw new Error(`Failed to reset password: ${err}`);
+    }
+  };
+
+  const checkUserProvider = (provider: string): boolean | null => {
+    if (currentUser) {
+      const matchedProvider = currentUser.identities.find(
+        (x) => x.providerType.toLowerCase() === provider.toLowerCase()
+      );
+      return matchedProvider ? true : false;
+    } else {
+      return null;
+    }
+  };
+
+  const wrapped = {
+    app,
+    currentUser,
+    logInEmail,
+    logInAnon,
+    logOut,
+    requestPasswordReset,
+    completePasswordReset,
+    checkUserProvider,
+  };
   return (
     <RealmAppContext.Provider value={wrapped}>
       {children}
